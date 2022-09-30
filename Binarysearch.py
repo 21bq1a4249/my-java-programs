a89456ab5c45193347b4e360d428d227be561298
21bq1a4249 <113082224+21bq1a4249@users.noreply.github.com>
2022-09-30 08:51:06 +0530

    Add files via upload

---
+++ Binarysearch.py
@@ -0,0 +1,19 @@
+def Binarysearch(l,low,high,key):
+     while low<=high:
+        mid=(low+high)//2
+        if l[mid]==key:
+            return mid
+        elif l[mid]>key:
+            high=mid-1
+        else:
+            low=mid+1
+     return -1
+l=[1,2,3,4,5,6,9]
+low=0
+high=len(l)
+key=int(input("enter any value"))
+result=Binarysearch(l,low,len(l)-1,key)
+if result==-1:
+    print("element not found")
+else:
+print("element fount at",result)
